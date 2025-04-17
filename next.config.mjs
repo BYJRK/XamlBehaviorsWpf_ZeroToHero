ecf6cd35527823b7b1002f4b6725ad66c2ef8a3d
BYJRK <suomikoto@outlook.com>
2025-04-17 17:30:41 +0800

    Update HomePage title and configure image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's2.loli.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default withMDX(config);
